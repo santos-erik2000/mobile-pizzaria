74cbfc56cfbdbe5c52b4e7dbe27fb8a3a8896393
Giovanni Lucas <giovannilucas050@gmail.com>
2021-10-12 17:12:39 -0300

    "Tela cadastro - parcial"

---
+++ app/src/main/java/com/example/platpizza/Cadastro1.java
@@ -0,0 +1,14 @@
+package com.example.platpizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro1);
+    }
+}
